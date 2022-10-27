b5a4d6f61e6199278ebc82a7095a36562b1a7377
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2022-10-27 12:10:08 +0200

    Add license

--- bot/src/main/java/de/chojo/gamejam/commands/server/plugins/Uninstall.java
+++ bot/src/main/java/de/chojo/gamejam/commands/server/plugins/Uninstall.java
@@ -1,3 +1,9 @@
+/*
+ *     SPDX-License-Identifier: AGPL-3.0-only
+ *
+ *     Copyright (C) 2022 DevCord Team and Contributor
+ */
+
 package de.chojo.gamejam.commands.server.plugins;
 
 import de.chojo.gamejam.commands.server.Server;
