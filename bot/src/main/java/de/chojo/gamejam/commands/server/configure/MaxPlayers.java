9a88d3e0f2cc177499c88f9f73e700c01247a55c
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2022-10-29 20:07:29 +0200

    Add license

--- bot/src/main/java/de/chojo/gamejam/commands/server/configure/MaxPlayers.java
+++ bot/src/main/java/de/chojo/gamejam/commands/server/configure/MaxPlayers.java
@@ -1,3 +1,9 @@
+/*
+ *     SPDX-License-Identifier: AGPL-3.0-only
+ *
+ *     Copyright (C) 2022 DevCord Team and Contributor
+ */
+
 package de.chojo.gamejam.commands.server.configure;
 
 import de.chojo.gamejam.commands.server.Server;
