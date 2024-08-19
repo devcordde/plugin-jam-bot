69bf8da9b0df8133c4fab0186a2e8692cd5f8edd
Lilly <46890129+RainbowDashLabs@users.noreply.github.com>
2024-08-20 01:04:40 +0200

    Adjust entrypoint to not run inside the shell

--- docker/resources/docker-entrypoint.sh
+++ docker/resources/docker-entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Xms256m -Xmx2048m -Dbot.config=config/config.json -Dlog4j.configurationFile=config/log4j2.xml -Dcjda.localisation.error.name=false -jar ./bot.jar
+exec java -Xms256m -Xmx2048m -Dbot.config=config/config.json -Dlog4j.configurationFile=config/log4j2.xml -Dcjda.localisation.error.name=false -jar ./bot.jar
