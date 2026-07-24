b2b3de3148ede83023114fea4e11df7678daf87c
TheZexquex <contact@thezexquex.dev>
2026-07-24 20:01:33 +0200

    Update License header to 2026

--- backend/src/main/java/de/chojo/pluginjam/database/entity/jam/JamTime.java
+++ backend/src/main/java/de/chojo/pluginjam/database/entity/jam/JamTime.java
@@ -1,7 +1,7 @@
 /*
  *     SPDX-License-Identifier: AGPL-3.0-only
  *
- *     Copyright (C) 2022 DevCord Team and Contributor
+ *     Copyright (C) 2026 DevCord Team and Contributor
  */
 
 package de.chojo.pluginjam.database.entity.jam;
